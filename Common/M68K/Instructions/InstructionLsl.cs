93822e1f4dc10dafc502c4b7b34b18fccbe15f08
Ricky Leverence Jr <ricky.leverence@gmail.com>
2025-01-04 16:16:26 -0500

    Fix LSL instruction.

--- Common/M68K/Instructions/InstructionLsl.cs
+++ Common/M68K/Instructions/InstructionLsl.cs
@@ -8,9 +8,11 @@ public class InstructionLsl : BaseInstruction
     private const string InstructionName = "LSL";
     private const int InstMask = 0b1111_0001_0000_0000;
     private const int InstMaskTarget = 0b1110_0001_0000_0000;
-    
-    private string _destRegister;
-    private string _srcAddress;
+
+    private bool _isRegisterShift;
+    private string _size;
+    private string _src;
+    private string _dest;
 
     public InstructionLsl(Hunk hunk, int hunkSectionNumber, ref int pc) : base(hunk, hunkSectionNumber, ref pc)
     {
@@ -23,37 +25,40 @@ public class InstructionLsl : BaseInstruction
 
     public override void ParseSpecificInstruction(Hunk hunk, int hunkSectionNumber, ref int pc)
     {
-        _destRegister = $"A{(Instruction >> 9) & 0b111}";
-
-        var addressingMode = (Instruction >> 3) & 0b111;
-        var addressingModeRegister = Instruction & 0b111;
-
-        switch (addressingMode)
+        // if size = 0b11 then memory shift, otherwise it's a register shift.
+        var sizeBits = (Instruction >> 6) & 0b11;
+        if (sizeBits == 0b11)
         {
-            case 0b111 when (addressingModeRegister & 0b001) == 0b001:
-                // Read 4 bytes in
-                ExtraInstructionBytes.Add(hunk.HunkSections[hunkSectionNumber].Data[pc + 0]);
-                ExtraInstructionBytes.Add(hunk.HunkSections[hunkSectionNumber].Data[pc + 1]);
-                ExtraInstructionBytes.Add(hunk.HunkSections[hunkSectionNumber].Data[pc + 2]);
-                ExtraInstructionBytes.Add(hunk.HunkSections[hunkSectionNumber].Data[pc + 3]);
-                pc += 4;
+            // memory shift.
+            _src = InstructionUtilities.ParseSourceAddress(Instruction, hunk, hunkSectionNumber, ref pc,
+                ExtraInstructionBytes);
+            _isRegisterShift = false;
+        }
+        else
+        {
+            // register shift.
+            var ir = (Instruction >> 5) & 0b1;
 
-                // Convert that to an address.
-                var address = ExtraInstructionBytes[0] * 256 * 256 * 256 +
-                              ExtraInstructionBytes[1] * 256 * 256 +
-                              ExtraInstructionBytes[2] * 256 +
-                              ExtraInstructionBytes[3];
+            _size = sizeBits switch
+            {
+                0b01 => ".W",
+                0b10 => ".L",
+                0b00 => ".B",
+                _ => $".Unknown_{sizeBits:b2}"
+            };
 
-                _srcAddress = $"${address:X8}";
-                break;
-            default:
-                _srcAddress = InstructionUtilities.ParseSourceAddress(Instruction, hunk, hunkSectionNumber, ref pc, ExtraInstructionBytes);
-                break;
+            var srcBits = (Instruction >> 9) & 0b111;
+            var srcCount = srcBits == 0 ? 8 : srcBits;
+            _src = $"#{srcCount}";
+            _dest = $"D{Instruction & 0b111}";
+            _isRegisterShift = true;
         }
     }
 
     public override string ToAssembly()
     {
-        return $"{InstructionName} {_srcAddress},{_destRegister}";
+        return _isRegisterShift
+            ? $"{InstructionName}{_size} {_src},{_dest}"
+            : $"{InstructionName} {_src}";
     }
 }
