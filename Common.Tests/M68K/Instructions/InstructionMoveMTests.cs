2983d286214d15de32ee25c039350bdf9fb51707
Ricky Leverence Jr <ricky.leverence@gmail.com>
2025-01-04 20:02:59 -0500

    More complex, but MOVEM is fixed now.

--- Common.Tests/M68K/Instructions/InstructionMoveMTests.cs
+++ Common.Tests/M68K/Instructions/InstructionMoveMTests.cs
@@ -5,6 +5,7 @@ namespace Common.Tests.M68K.Instructions;
 public class InstructionMoveMTests
 {
     [TestCase("MOVEM.L D1-D6/A0-A6,-(A7)", "48E77EFE")]
+    [TestCase("MOVEM.L (A7)+,D1-D6/A0-A6", "4CDF7F7E")]
     public void RunFullTest(string assembly, string bytesString)
     {
         TestHelpers.RunFullTest(assembly, bytesString);
--- Common/M68K/Instructions/InstructionMoveM.cs
+++ Common/M68K/Instructions/InstructionMoveM.cs
@@ -5,10 +5,11 @@ namespace Common.M68K.Instructions;
 public sealed class InstructionMoveM : BaseInstruction
 {
     private const string InstructionName = "MOVEM";
-    private const int InstMask = 0b1111111110000000;
-    private const int InstMaskTarget = 0b0100100010000000;
+    private const int InstMask = 0b1111_1011_1000_0000;
+    private const int InstMaskTarget = 0b0100_1000_1000_0000;
 
     private readonly List<string> _registers;
+    private int _dr;
     private string _size;
     private string _operationMemoryAddress;
     
@@ -24,60 +25,112 @@ public sealed class InstructionMoveM : BaseInstruction
 
     public override void ParseSpecificInstruction(Hunk hunk, int hunkSectionNumber, ref int pc)
     {
-        var operationMemoryMode = (Instruction >> 3) & 7;
-        var operationMemoryAddress = (Instruction & 7).ToString();
-        _operationMemoryAddress = operationMemoryMode switch
-        {
-            0b100 => $"-(A{operationMemoryAddress})",
-            0b010 => $"(A{operationMemoryAddress})",
-            _ => operationMemoryAddress
-        };
-        
+        // dr, 0 = register to memory (predecrement); 1 = memory to register (postincrement)
+        _dr = (Instruction >> 10) & 0b1;
+
+        _operationMemoryAddress =
+            InstructionUtilities.ParseSourceAddress(Instruction, hunk, hunkSectionNumber, ref pc,
+                ExtraInstructionBytes);
+
         ExtraInstructionBytes.Add(hunk.HunkSections[hunkSectionNumber].Data[pc]);
         ExtraInstructionBytes.Add(hunk.HunkSections[hunkSectionNumber].Data[pc + 1]);
         pc += 2;
 
-        foreach (var pair in new[] { ("D", ExtraInstructionBytes[0]), ("A", ExtraInstructionBytes[1]) })
+        var targetByteA = _dr == 0 ? ExtraInstructionBytes[0] : ExtraInstructionBytes[1];
+        var targetByteB = _dr == 0 ? ExtraInstructionBytes[1] : ExtraInstructionBytes[0];
+
+        foreach (var pair in new[]
+                 {
+                     ("D", targetByteA),
+                     ("A", targetByteB)
+                 })
         {
             var targetRegisterId = pair.Item1;
             var targetRegister = pair.Item2;
-            
-            // This is pretty messy.
-            int? firstConsecutive = null;
-            for (var i = 8; i >= 0; i--)
+
+            if (_dr == 0)
             {
-                if (i == 8)
-                {
-                    if (!firstConsecutive.HasValue)
-                        continue;
-
-                    _registers.Add(firstConsecutive.Value != 7
-                        ? $"{targetRegisterId}{firstConsecutive.Value}-D7"
-                        : $"{targetRegisterId}7");
-                }
-                else if (((targetRegister >> i) & 1) == 1)
-                {
-                    firstConsecutive ??= 7 - i;
-                }
-                else
-                {
-                    if (!firstConsecutive.HasValue)
-                        continue;
-                    
-                    var lastValue = 7 - i - 1;
-
-                    _registers.Add(lastValue - firstConsecutive.Value == 0
-                        ? $"{targetRegisterId}{firstConsecutive.Value}"
-                        : $"{targetRegisterId}{firstConsecutive.Value}-{targetRegisterId}{lastValue}");
-                }
+                ParseMaskPostIncrement(targetRegister, targetRegisterId);
+            }
+            else
+            {
+                ParseMaskPreDecrement(targetRegister, targetRegisterId);
             }
         }
-        
+
         _size = (Instruction & 0b1000000) == 0b1000000 ? "L" : "W";
+    }
+
+    private void ParseMaskPostIncrement(byte targetRegister, string targetRegisterId)
+    {
+        int? firstConsecutive = null;
+        for (var i = 8; i >= 0; i--)
+        {
+            if (i == 8)
+            {
+                if (!firstConsecutive.HasValue)
+                    continue;
+
+                _registers.Add(firstConsecutive.Value != 7
+                    ? $"{targetRegisterId}{firstConsecutive.Value}-{targetRegisterId}7"
+                    : $"{targetRegisterId}7");
+            }
+            else if (((targetRegister >> i) & 1) == 1)
+            {
+                firstConsecutive ??= 7 - i;
+            }
+            else
+            {
+                if (!firstConsecutive.HasValue)
+                    continue;
+
+                var lastValue = 7 - i - 1;
+
+                _registers.Add(lastValue - firstConsecutive.Value == 0
+                    ? $"{targetRegisterId}{firstConsecutive.Value}"
+                    : $"{targetRegisterId}{firstConsecutive.Value}-{targetRegisterId}{lastValue}");
+            }
+        }
+    }
+
+    private void ParseMaskPreDecrement(byte targetRegister, string targetRegisterId)
+    {
+        int? firstConsecutive = null;
+        for (var i = 0; i <= 8; i++)
+        {
+            if (i == 8)
+            {
+                if (!firstConsecutive.HasValue)
+                    continue;
+
+                _registers.Add(firstConsecutive.Value != 7
+                    ? $"{targetRegisterId}{firstConsecutive.Value}-{targetRegisterId}7"
+                    : $"{targetRegisterId}7");
+            }
+            else if (((targetRegister >> i) & 1) == 1)
+            {
+                firstConsecutive ??= i;
+            }
+            else
+            {
+                if (!firstConsecutive.HasValue)
+                    continue;
+
+                var lastValue = i - 1;
+
+                _registers.Add(lastValue - firstConsecutive.Value == 0
+                    ? $"{targetRegisterId}{firstConsecutive.Value}"
+                    : $"{targetRegisterId}{firstConsecutive.Value}-{targetRegisterId}{lastValue}");
+
+                firstConsecutive = null;
+            }
+        }
     }
 
     public override string ToAssembly()
     {
-        return $"{InstructionName}.{_size} {string.Join("/", _registers)},{_operationMemoryAddress}";
+        return _dr == 0
+            ? $"{InstructionName}.{_size} {string.Join("/", _registers)},{_operationMemoryAddress}"
+            : $"{InstructionName}.{_size} {_operationMemoryAddress},{string.Join("/", _registers)}";
     }
 }
