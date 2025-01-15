df1f99f48ad5fdd2aa25b6cbde820d8699a53e72
Ricky Leverence Jr <ricky.leverence@gmail.com>
2025-01-15 15:06:17 -0500

    More tweaks.

--- Common.Tests/M68K/Instructions/InstructionBccTests.cs
+++ Common.Tests/M68K/Instructions/InstructionBccTests.cs
@@ -2,11 +2,12 @@ using System.Reflection;
 
 namespace Common.Tests.M68K.Instructions;
 
-public class InstructionBrccTests
+public class InstructionBccTests
 {
     [TestCase("BRA.S 2", "6002")]
     [TestCase("BNE.S 6", "6606")]
     [TestCase("BGE.S 8", "6C08")]
+    [TestCase("BRA.W LAB_4324C", "6000FF72")]
     public void RunFullTest(string assembly, string bytesString)
     {
         TestHelpers.RunFullTest(assembly, bytesString);
