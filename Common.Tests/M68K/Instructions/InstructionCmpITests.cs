25911687b00fd5815f51fff80ddf06f0537e10ca
Ricky Leverence Jr <ricky.leverence@gmail.com>
2025-01-15 14:55:15 -0500

    adding another broken test.

--- Common.Tests/M68K/Instructions/InstructionCmpITests.cs
+++ Common.Tests/M68K/Instructions/InstructionCmpITests.cs
@@ -3,6 +3,7 @@ namespace Common.Tests.M68K.Instructions;
 public class InstructionCmpITests
 {
     [TestCase("CMPI.L #$000927C0,D0", "0C80000927C0")]
+    [TestCase("CMPI.L #$00000020,$5982(A4)", "0CAC000000205982")]
     public void RunFullTest(string assembly, string bytesString)
     {
         TestHelpers.RunFullTest(assembly, bytesString);
