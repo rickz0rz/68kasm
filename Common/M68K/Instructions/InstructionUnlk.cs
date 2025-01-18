0b59c5f53cd6ecedcab69ff7c8399a65b2a3b605
Ricky Leverence Jr <ricky.leverence@gmail.com>
2025-01-17 19:14:13 -0500

    Unlink

---
+++ Common/M68K/Instructions/InstructionUnlk.cs
@@ -0,0 +1,33 @@
+using Common.Amiga;
+using Common.M68K.Addresses;
+
+namespace Common.M68K.Instructions;
+
+public class InstructionUnlk : BaseInstruction
+{
+    private const string InstructionName = "UNLK";
+    private const int InstMask = 0b1111_1111_1111_1000;
+    private const int InstMaskTarget = 0b0100_1110_0101_1000;
+
+    private AddressRegister _register;
+
+    public InstructionUnlk(Hunk hunk, int hunkSectionNumber, ref int pc) : base(hunk, hunkSectionNumber, ref pc)
+    {
+    }
+
+    public static bool IsInstruction(int instruction)
+    {
+        return (instruction & InstMask) == InstMaskTarget;
+    }
+
+    public override void ParseSpecificInstruction(Hunk hunk, int hunkSectionNumber, ref int pc)
+    {
+        var registerBits = Instruction & 0b111;
+        _register = new AddressRegister(registerBits);
+    }
+
+    public override string ToAssembly()
+    {
+        return $"{InstructionName} {_register}";
+    }
+}
